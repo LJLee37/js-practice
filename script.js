2616349e18ad5e4ae749040ed6ecf09c605cc42d
LJLee37 <oh-my-zsh@kakao.com>
2020-02-16 23:13:01 +0900

    16th Feb learning

--- script.js
+++ script.js
@@ -115,7 +115,39 @@ window.addEventListener("online", handleOnline);
 
 */
 
+const title = document.getElementById("title");
 
+const CLICKED_CLASS = "clicked";
 
+/*
+function handleClick(){
+    const currentClass = title.className;
+    if(currentClass !== CLICKED_CLASS){
+        title.className = CLICKED_CLASS;
+    }
+    else{
+        title.className = "";
+    }
+}
+*/
+/*
+function handleClick(){
+    const currentClass = title.className;
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if(!hasClass){
+        title.classList.add(CLICKED_CLASS);
+    }
+    else{
+        title.classList.remove(CLICKED_CLASS);
+    }
+}
+*/
 
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+}
 
+function init(){
+    title.addEventListener("click", handleClick);
+}
+init();
